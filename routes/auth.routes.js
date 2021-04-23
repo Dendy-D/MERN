dc6167923e59f4ca3333e353e6f05e744c82fbe9
Dendy-D <daniilvasilenko2@mail.com>
2021-04-23 10:01:29 +0300

    Create is base structure back-end and mongo

---
+++ routes/auth.routes.js
@@ -0,0 +1,19 @@
+const {Router} = require('express')
+const router = Router()
+const User = require('../models/User')
+
+// /api/auth/register
+router.post('/register', async (req, res) => {
+  try {
+    const {email, password} = req.body
+  } catch (e) {
+    res.status(500).json({message: 'Something wrong'})
+  }
+})
+
+// /api/auth/login
+router.post('/login', async (req, res) => {
+
+})
+
+module.exports = router
